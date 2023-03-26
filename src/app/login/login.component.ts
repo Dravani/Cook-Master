9e49278d74b722bd17fcce70d9d69250a28d7be9
Dravani <42421220+Dravani@users.noreply.github.com>
2023-03-26 17:11:22 -0400

    Different Login Components

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FirebaseTSAuth } from 'firebasets/firebasetsAuth/firebaseTSAuth';
 
 @Component({
   selector: 'app-login',
@@ -6,16 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  loginValue:boolean;
-
-  constructor(){ 
-    this.loginValue = true;
-  }
-
-  get LoginValue(){
-
-    return this.loginValue;
+  state = AuthenticatorCompState.LOGIN;
+  firebasetsAuth: FirebaseTSAuth;
+  constructor(){
+    this.firebasetsAuth = new FirebaseTSAuth();
   }
   
 
+  onForgotPasswordClick(){
+    this.state = AuthenticatorCompState.FORGOT_PASSWORD;
+
+  }
+  
+  onCreateAccountClick(){
+    this.state = AuthenticatorCompState.REGISTER;
+
+  }
+  
+  onLoginClick(){
+    this.state = AuthenticatorCompState.LOGIN;
+
+  }
+
+  isLoginState(){
+    return this.state == AuthenticatorCompState.LOGIN;
+  }
+
+  isRegisterState(){
+    return this.state == AuthenticatorCompState.REGISTER;
+  }
+
+  isForgotPasswordState(){
+    return this.state == AuthenticatorCompState.FORGOT_PASSWORD;
+  }
+
+  getStateText(){
+      switch(this.state){
+      case AuthenticatorCompState.LOGIN:
+        return "Login Page";
+      case AuthenticatorCompState.REGISTER:
+        return "Sign Up";
+      case AuthenticatorCompState.FORGOT_PASSWORD:
+        return "Forgot Password";
+    }
+}
+
+  
+}
+
+export enum AuthenticatorCompState {
+  LOGIN,
+  REGISTER,
+  FORGOT_PASSWORD
 }
