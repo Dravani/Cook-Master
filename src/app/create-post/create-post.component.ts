b176349af2e85f258995953a11a16398e3c1ad18
EdwinASalcedo <e.salcedo@ufl.edu>
2023-03-26 16:46:07 -0400

    createpost functionality
    Should upload to firebase

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { FirebaseTSAuth } from 'firebasets/firebasetsAuth/firebaseTSAuth';
 import { FirebaseTSFirestore } from 'firebasets/firebasetsFirestore/firebaseTSFirestore';
 import { FirebaseTSStorage } from 'firebasets/firebasetsStorage/firebaseTSStorage';
 import { FirebaseTSApp } from 'firebasets/firebasetsApp/firebaseTSApp';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-create-post',
@@ -13,22 +15,60 @@ export class CreatePostComponent {
   auth = new FirebaseTSAuth();
   firestore = new FirebaseTSFirestore();
   storage = new FirebaseTSStorage();
-  constructor() { 
+  constructor(private dialog: MatDialogRef<CreatePostComponent>) { 
     this.selectedImageFile = null as any;
   }
 
   onPostClick(commentInput : HTMLTextAreaElement) {
     let comment = commentInput.value;
+    if(this.selectedImageFile) {
+      this.uploadImagePost(comment);
+    }
+    else {
+      this.uploadPost(comment);
+    }
+  }
+
+  uploadImagePost(comment: string) {
     let postId = this.firestore.genDocId();
     this.storage.upload(
       {
         uploadName: "upload Image Post",
         path: ["Posts", postId, "image"],
         data: {
-          data.this.selectedImageFile
+          data: this.selectedImageFile
         },
-        onComplete: (downloadUrl) => {
-          alert(downloadUrl);
+        onComplete: (downloadUrl: any) => {
+          this.firestore.create(
+            {
+              path: ["Posts", postId],
+              data: {
+                comment: comment,
+                creatorId: this.auth.getAuth().currentUser.uid,
+                imageUrl: downloadUrl,
+                timestamp: FirebaseTSApp.getFirestoreTimestamp()
+              },
+              onComplete: (docId: any) => {
+                this.dialog.close();
+              }
+            }
+          );
+        }
+      }
+    );
+  }
+
+  uploadPost(comment: string) {
+    this.firestore.create(
+      {
+        path: ["Posts"],
+        data: {
+          comment: comment,
+          creatorId: this.auth.getAuth().currentUser.uid,
+          timestamp: FirebaseTSApp.getFirestoreTimestamp()
+        },
+        onComplete: (docId: any) => {
+          this.dialog.close();
         }
       }
     );
@@ -36,20 +76,16 @@ export class CreatePostComponent {
 
   onPhotoSelected(photoSelector: HTMLInputElement) {
     this.selectedImageFile = photoSelector.files![0];
+    if(!this.selectedImageFile) return;
     let fileReader = new FileReader(); 
-
-    if(!this.selectedImageFile) return;  
-      fileReader.readAsDataURL(this.selectedImageFile);
-      fileReader.addEventListener(
-        "loadend",
-        ev => {
-          let readableString = fileReader.result!.toString();
-          let postPreviewImage = <HTMLImageElement>document.getElementById("post-preview-image");
-          postPreviewImage.src = readableString;
-        } 
-      );
-}
-
-
-
+    fileReader.readAsDataURL(this.selectedImageFile);
+    fileReader.addEventListener(
+      "loadend",
+      ev => {
+        let readableString = fileReader.result!.toString();
+        let postPreviewImage = <HTMLImageElement>document.getElementById("post-preview-image");
+        postPreviewImage.src = readableString;
+      } 
+    );
+  }
 }
