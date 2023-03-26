7935211c0750e61f060162671f0616807ddf0ded
Dravani <42421220+Dravani@users.noreply.github.com>
2023-03-26 16:06:04 -0400

    Added Firebase Database

---
+++ src/environment/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+production: false,
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebaseConfig: {
+    apiKey: "AIzaSyBpc9oOfQSr0hX-PWGbPzL_E8vV1oHUGR0",
+    authDomain: "cookmaster-5fadc.firebaseapp.com",
+    projectId: "cookmaster-5fadc",
+    storageBucket: "cookmaster-5fadc.appspot.com",
+    messagingSenderId: "261439498569",
+    appId: "1:261439498569:web:9a1a34bcd2c37e8b085298",
+    measurementId: "G-RETBJP6TCT"
+  }
+};
