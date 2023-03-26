09ef41d308e0e2c67d1f87adf31671d3cf67d05d
EdwinASalcedo <e.salcedo@ufl.edu>
2023-03-26 16:09:18 -0400

    firebase for createPost
    storing images on firebase

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FirebaseTSFirestore } from 'firebasets/firebasetsFirestore/firebaseTSFirestore';
+import { FirebaseTSStorage } from 'firebasets/firebasetsStorage/firebaseTSStorage';
+import { FirebaseTSApp } from 'firebasets/firebasetsApp/firebaseTSApp';
 
 @Component({
   selector: 'app-create-post',
@@ -7,10 +10,29 @@ import { Component } from '@angular/core';
 })
 export class CreatePostComponent {
   selectedImageFile: File;
+  auth = new FirebaseTSAuth();
+  firestore = new FirebaseTSFirestore();
+  storage = new FirebaseTSStorage();
   constructor() { 
     this.selectedImageFile = null as any;
   }
 
+  onPostClick(commentInput : HTMLTextAreaElement) {
+    let comment = commentInput.value;
+    let postId = this.firestore.genDocId();
+    this.storage.upload(
+      {
+        uploadName: "upload Image Post",
+        path: ["Posts", postId, "image"],
+        data: {
+          data.this.selectedImageFile
+        },
+        onComplete: (downloadUrl) => {
+          alert(downloadUrl);
+        }
+      }
+    );
+  }
 
   onPhotoSelected(photoSelector: HTMLInputElement) {
     this.selectedImageFile = photoSelector.files![0];
