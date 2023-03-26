74a51d7a51c840c111dcff493741261545dc4f91
EdwinASalcedo <e.salcedo@ufl.edu>
2023-03-26 17:16:13 -0400

    set up storage in fb
    firebase storage configuration

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -44,7 +44,7 @@ export class CreatePostComponent {
               path: ["Posts", postId],
               data: {
                 comment: comment,
-                creatorId: this.auth.getAuth().currentUser.uid,
+                creatorId: this.auth.getAuth().currentUser!.uid,
                 imageUrl: downloadUrl,
                 timestamp: FirebaseTSApp.getFirestoreTimestamp()
               },
@@ -64,7 +64,7 @@ export class CreatePostComponent {
         path: ["Posts"],
         data: {
           comment: comment,
-          creatorId: this.auth.getAuth().currentUser.uid,
+          creatorId: this.auth.getAuth().currentUser!.uid,
           timestamp: FirebaseTSApp.getFirestoreTimestamp()
         },
         onComplete: (docId: any) => {
